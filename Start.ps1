a241d3379db127651ce10a7469b914791ba0905b
TwistedBobRoss <arpaypal300blk@gmail.com>
2026-06-14 19:33:31 -0400

    Start SuperTuxKart from x86_64 package directory

--- Start.ps1
+++ Start.ps1
@@ -1,7 +1,8 @@
 $ErrorActionPreference = "Stop"
 
-$exe = "C:\stk\supertuxkart.exe"
+$exe = "C:\stk\stk-code\build-x86_64\bin\supertuxkart.exe"
 $config = "C:\serverfiles\server_config.xml"
+$workingDirectory = Split-Path -LiteralPath $exe -Parent
 
 if (-not (Test-Path -LiteralPath $exe)) {
     throw "Could not find $exe"
@@ -11,15 +12,30 @@ if (-not (Test-Path -LiteralPath $config)) {
     throw "Could not find $config. Mount or create C:\serverfiles\server_config.xml before starting."
 }
 
+function Invoke-Stk {
+    param([string[]]$Arguments)
+
+    Push-Location -LiteralPath $workingDirectory
+    try {
+        & $exe @Arguments
+        $exitCode = $LASTEXITCODE
+    }
+    finally {
+        Pop-Location
+    }
+
+    return $exitCode
+}
+
 $username = $env:STK_USERNAME
 $password = $env:STK_PASSWORD
 
 if (-not [string]::IsNullOrWhiteSpace($username) -and -not [string]::IsNullOrWhiteSpace($password)) {
-    & $exe --init-user "--login=$username" "--password=$password"
-    if ($LASTEXITCODE -ne 0) {
-        throw "SuperTuxKart account initialization failed with exit code $LASTEXITCODE"
+    $exitCode = Invoke-Stk @("--init-user", "--login=$username", "--password=$password")
+    if ($exitCode -ne 0) {
+        throw "SuperTuxKart account initialization failed with exit code $exitCode"
     }
 }
 
-& $exe "--server-config=$config"
-exit $LASTEXITCODE
+$exitCode = Invoke-Stk @("--server-config=$config")
+exit $exitCode
