4ebc5071cdfcf80a2866df955e9753c7ec4c3191
TwistedBobRoss <arpaypal300blk@gmail.com>
2026-06-14 17:16:28 -0400

    Add local Docker run example

---
+++ docker-run.example.ps1
@@ -0,0 +1,18 @@
+$Image = "ghcr.io/twistedbobross/supertuxkart-gsa-windows:1.5-ltsc2022"
+$ContainerName = "twisted-supertuxkart"
+$ServerFiles = "C:\stk-test\serverfiles"
+
+New-Item -ItemType Directory -Force -Path $ServerFiles | Out-Null
+
+if (-not (Test-Path -LiteralPath (Join-Path $ServerFiles "server_config.xml"))) {
+    Copy-Item -Path ".\server_config.xml" -Destination (Join-Path $ServerFiles "server_config.xml")
+}
+
+docker run --name $ContainerName `
+  -d `
+  -p 2757:2757/udp `
+  -p 2759:2759/udp `
+  -v "${ServerFiles}:C:\serverfiles" `
+  -e STK_USERNAME="" `
+  -e STK_PASSWORD="" `
+  $Image
